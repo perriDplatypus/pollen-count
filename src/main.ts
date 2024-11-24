a554ae81480761497c3804a69e95f43d458d1e1f
abhay <abhaytyagi2513@gmail.com>
2024-11-24 17:43:14 +1100

    removed typescript scraper

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import pollenScrapper from './pollenScraper'
-pollenScrapper()
+
 const app = createApp(App)
 
 app.use(router)
